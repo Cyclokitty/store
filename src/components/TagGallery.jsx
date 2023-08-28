72a8ea079b349232099fc3c206ae3f61d4d5f369
Laura Veee <laura@vingada.com>
2023-08-28 12:50:16 -0400

    changed taggallery page to use bootstrap for the cards

--- src/components/TagGallery.jsx
+++ src/components/TagGallery.jsx
@@ -1,30 +1,23 @@
-// this will hold all of the TagPage.jsx cards
-// they will display the categories/tags from the mongodb db
-
 import { Link } from 'react-router-dom';
-import Card from "./Card";
-import '../App.css';
+import Card from 'react-bootstrap/Card'
 
-export default function TagGallery({ tags, tagRoute }) {
-
+export default function TagGallery2({ tags, tagRoute }) {
     return (
-        <ul className="gallery">
-        {tags.map((tag, id) => (
-            <li key={id}>
-                <Card
-                    style={{width: '450px', height: '550px'}}
-                    avatar={<img alt='store mascot' src={tag.variants[0].img} style={{width: '100%', paddingTop: '20px'}}/>}
-                    title={tag.name}                   
-                >
-                    <p>{tag.description}</p>
-                    <p>Available in {tag.variants.length} colours.</p>
+        <ul className='gallery'>
+            {tags.map((tag, id) => (
+                <li key={id}>
+                <Card  style={{width: '18rem', height: '30rem', padding: '10px'}}>
+                    <Card.Title>{tag.name}</Card.Title>
+                    <Card.Img src={tag.variants[0].img} />
+                    <Card.Text>{tag.description}</Card.Text>
+                    <Card.Text>Available in {tag.variants.length} colours.</Card.Text>
                     <Link
                         to={`/${tagRoute}/${tag.name}`}
                         state={{ item: {tag}}}
                     ><h5 style={{color: '#400040', textAlign: 'center'}}>Click here to see {tag.name} products!</h5></Link>
                 </Card>
-            </li>
-        ))}
-      </ul>
+                </li>
+            ))}
+        </ul>
     )
 }
